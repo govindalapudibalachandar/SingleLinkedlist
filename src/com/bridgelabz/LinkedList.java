0c4eb394892d05abbe13bfbcf2c3ed3b40017203
balachandar <govindalapudibalachandar@gmail.com>
2021-09-07 18:56:14 +0530

    Added:AddingData file is added

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -2,15 +2,44 @@ package com.bridgelabz;
 
 public class LinkedList {
 	
-	Node head; 
+	Node head;       // first node itself is head
+	
+	// Add Method
     public void Add(int data)
     {
         Node node = new Node(data);
+        node.data = data;
+        node.next=null;
         
-       System.out.println("{0} inserted into linked list"+ node.data);
+        if (head==null)  // if it first object is head it should be null
+        {
+        	head = node;
+        }
+        else {
+            Node  n = head ;   // n is a temparory node to check if it is a null
+        	while (n.next != null) {
+        	  n = n.next;
+        	}
+        	n.next = node;
+        	}
     }
+       // display method 
+       public void  Display() {
+    	  Node node = head;
+    	  while (node.next!= null)
+    	  {
+    		  System.out.println(node.data);
+    		  
+    		 // after printing ever node we must shift to another node
+    		  node = node.next; }
+    	  System.out.println(node.data);
+        
+      }
+  }
    
-    }
+    
+   
+    
 
 
 
--- src/com/bridgelabz/Test.java
+++ src/com/bridgelabz/Test.java
@@ -7,17 +7,11 @@ public class Test {
 		 {
 	         System.out.println("Welcome to Linked List operations");
 	         LinkedList list = new LinkedList();
-	            list.Add(56);
-	            list.Add(30);
-	            list.Add(70);
-	         //  int a = list.Search(30);
-	           //  list.InsertAtParticularPosition(a+1, 40);
-	            //list.Display();
-	       //  LinkedList list = new LinkedList();
-	         //list.Add(56);
-	         //list.Add(30);
-	         //list.Add(70);
-	         
+	           list.Add(56);
+	           list.Add(30);
+	           list.Add(70);
+	           
+	           list.Display();	            
 	}
 
 	}
